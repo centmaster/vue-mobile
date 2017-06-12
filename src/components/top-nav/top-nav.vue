3f97c1dd815eb174e029f93d5ca0813a08e258d8
centmaster <861676131@qq.com>
2017-06-12 22:32:25 +0800

    route

--- src/components/top-nav/top-nav.vue
+++ src/components/top-nav/top-nav.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="hello">
+  <div class="nav">
+
     <button>{{ msg }}</button>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'hello',
     data () {
@@ -17,21 +19,9 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+ button{
+   width:150px;
+   height: 40px;
+   margin: 0 auto;
+ }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/route-top-nav'
+import content from '@/components/content/content'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'content',
+      component: content
     }
   ]
 })
